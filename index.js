ae0a7c2260bee52a9099343093f847ff86e99e74
Roshan Paturkar <paturkarr@gmail.com>
2024-08-02 16:25:49 +0530

    cryptify-js initial release

---
+++ index.js
@@ -0,0 +1,13 @@
+/*!
+* Cryptify-JS v1.0.0
+* Copyright(c) 2024 Roshan K. Paturkar
+* MIT Licensed
+*/
+
+'use strict';
+
+// Importing the CRYPTIFYJS module
+const CRYPTIFYJS = require("./lib/cryptifyjs");
+
+// Exporting the CRYPTIFYJS module
+exports = module.exports = CRYPTIFYJS;
